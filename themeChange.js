6b9465462015fb6132661f387a031d703015ef8a
Name <krishnasarathishem@gmail.com>
2023-08-04 18:59:49 +0530

    Update themeChange.js

--- themeChange.js
+++ themeChange.js
@@ -1,7 +1,8 @@
 function setTheme() {
-    const root = document.documentElement;
-    const newTheme = root.className === 'dark' ? 'light' : 'dark';
-    root.className = newTheme;
+  const root = document.documentElement;
+  const newTheme = root.className === 'dark' ? 'light' : 'dark';
+  root.className = newTheme;
+  chrome.runtime.sendMessage({ target: "setTheme", theme: newTheme })
   }
   
   document.querySelector('.theme-toggle').addEventListener('click', setTheme)
